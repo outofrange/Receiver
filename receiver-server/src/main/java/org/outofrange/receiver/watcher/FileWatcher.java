6948df146054580c570723e02ae22fdff7fbd3b7
outofrange <markus.moeslinger@gmail.com>
2014-07-10 22:12:34 +0200

    - use path instead of file

--- receiver-server/src/main/java/org/outofrange/receiver/watcher/FileWatcher.java
+++ receiver-server/src/main/java/org/outofrange/receiver/watcher/FileWatcher.java
@@ -4,11 +4,8 @@ import org.outofrange.receiver.util.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.Path;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
 import java.util.Calendar;
@@ -24,31 +21,36 @@ public class FileWatcher implements Runnable {
     @Override
     public void run() {
         if (cachedMinutes > 0) {
-            checkFiles(new File(path));
+            checkFiles(Paths.get(path));
+        } else {
+            logger.trace("FileWatcher disabled - won't delete any files");
         }
     }
 
-    private void checkFiles(File directory) {
-        logger.debug("Searching for file older than " + cachedMinutes + " in directory " + directory.getAbsolutePath());
-        // TODO use Path instead of File
+    private void checkFiles(Path directory) {
+        logger.trace("Searching for file older than " + cachedMinutes + " minutes in directory " + directory.toAbsolutePath());
 
-        for (File file : directory.listFiles()) {
-            if (file.isFile()) {
-                Path path = file.toPath();
-                try {
-                    FileTime creationTime = Files.readAttributes(path, BasicFileAttributes.class, LinkOption.NOFOLLOW_LINKS).creationTime();
-                    if (oldEnough(creationTime.toMillis())) {
-                        logger.debug("Found file older than " + cachedMinutes + ": " + path.toAbsolutePath().toString());
-                        Files.delete(path);
+        try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(directory)) {
+            for (Path file : dirStream) {
+                if (Files.isRegularFile(file, LinkOption.NOFOLLOW_LINKS)) {
+                    try {
+                        FileTime creationTime = Files.readAttributes(file, BasicFileAttributes.class, LinkOption.NOFOLLOW_LINKS).creationTime();
+                        if (oldEnough(creationTime.toMillis())) {
+                            logger.debug("Found file older than " + cachedMinutes + ": " + file.toAbsolutePath().toString());
+                            Files.delete(file);
+                        }
+
+                    } catch (IOException e) {
+                        logger.error("Couldn't delete file " + file.getFileName() + "!", e);
                     }
 
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
-
             }
+        } catch (IOException e) {
+            logger.error("Couldn't list files of " + directory);
         }
     }
+
 
     private boolean oldEnough(long time) {
         long now = Calendar.getInstance().getTimeInMillis();

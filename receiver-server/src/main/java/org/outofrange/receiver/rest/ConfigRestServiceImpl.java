b1e5a45a3b1727c9f1dce0f187db526093cba238
outofrange <markus.moeslinger@gmail.com>
2014-07-10 21:01:13 +0200

    - changed pom (variables + dependencies) - removed javax.ws.rs annotations from service implementations \o/

--- receiver-server/src/main/java/org/outofrange/receiver/rest/ConfigRestServiceImpl.java
+++ receiver-server/src/main/java/org/outofrange/receiver/rest/ConfigRestServiceImpl.java
@@ -3,26 +3,18 @@ package org.outofrange.receiver.rest;
 import com.google.gson.Gson;
 import org.outofrange.receiver.service.ConfigService;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-@Path("/config")
+
 public class ConfigRestServiceImpl implements org.outofrange.receiver.rest.ConfigRestService {
 	private ConfigService service = new ConfigService();
 
     @Override
-    @GET
-	@Path("/qr")
-	@Produces("image/gif")
 	public Response getConfigAsQr() {
 		return Response.ok(service.getConfigAsQr()).build();
 	}
 
     @Override
-    @GET
-	@Produces("application/json")
 	public Response getConfig() {
 		return Response.ok().entity(new Gson().toJson(service.getConfigDto())).build();
 	}
--- receiver-server/src/main/java/org/outofrange/receiver/rest/FileRestServiceImpl.java
+++ receiver-server/src/main/java/org/outofrange/receiver/rest/FileRestServiceImpl.java
@@ -1,31 +1,22 @@
 package org.outofrange.receiver.rest;
 
 
-import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.outofrange.receiver.service.FileService;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.io.InputStream;
 
-@Path("/file/{fileName}")
 public class FileRestServiceImpl implements FileRestService {
     private FileService service = new FileService();
 
     @Override
-    @GET
-    @Produces(MediaType.APPLICATION_OCTET_STREAM)
-    public Response getFile(@PathParam("fileName") String fileName) {
+    public Response getFile(String fileName) {
         return Response.ok(service.getFile(fileName)).header("Content-Disposition", "attachment; filename=\"" + fileName + "\"").build();
     }
 
     @Override
-    @PUT
-    @Produces(MediaType.TEXT_PLAIN)
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response postFile(@PathParam("fileName") String fileId, @FormDataParam("file") InputStream fileInputStream) throws IOException {
+    public Response postFile(String fileId, InputStream fileInputStream) throws IOException {
         service.saveFile(fileInputStream, fileId);
 
         return Response.ok("File successfully saved").build();
